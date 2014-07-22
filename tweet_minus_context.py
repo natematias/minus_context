879eaca278a17bc26273de87892ef711dca96afb
Nathan Matias <natematias@gmail.com>
2014-07-22 16:33:54 -0400

    added sample config file

--- tweet_minus_context.py
+++ tweet_minus_context.py
@@ -1,7 +1,9 @@
 import nltk
-import feedburner
+import feedparser
 import random
 from HTMLParser import HTMLParser
+import twitter
+import yaml
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -12,23 +14,46 @@ class MLStripper(HTMLParser):
     def get_data(self):
         return ''.join(self.fed)
 
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
+def strip_tags(post):
+    if 'content' in post:
+      html = post['content']
+    elif 'summary' in post:
+      html = post['summary']
+    else:
+      html =""
+    try:
+      s = MLStripper()
+      s.feed(html)
+      return s.get_data()
+    except:
+      return ""
  
-# download a feed
-feed_url = "http://feeds.feedburner.com/saramwatson"
-feed = feedburner.parse(feed_url)
+# download a feed from Berkman Planet
+feed_url = "http://cyber.law.harvard.edu/planet/current/atom.xml"
+feed = feedparser.parse(feed_url)
 
-posts = [strip_tags(x['summary']) for x in feed['entries']]
+# create a list of posts
+posts = [strip_tags(x) for x in feed['entries']]
+
+#load sentence tokenizer
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
 sentences = []
 
+#for each post and each tweet length sentence, add to candidates9
 for post in posts:
   for sentence in sent_detector.tokenize(post.strip()):
-    if len(sentence) <=140:
+    if len(sentence) <=140 and len(sentence) > 50:
       sentences.append(sentence)
 
-print(random.choice(sentences))
+api_key = yaml.load(open('config.yaml'))
+
+api = twitter.Api(consumer_key=api_key['api'],
+  consumer_secret=api_key['consumer_secret'],
+  access_token_key=api_key['access_token_key'],
+  access_token_secret=api_key['access_token_secret'])
+
+# Tweet out a random sentence
+tweet = random.choice(sentences)
+print tweet
+api.PostUpdate(tweet)
